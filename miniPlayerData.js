fa2310b6f2b7e90757fb4eaef5beaab0dfd1273f
verdalia <govern.verdalia@gmail.com>
2020-05-22 19:31:12 +0200

    Arxius web verdàlia.cat
    Arxius del projecte "Verdàlia.cat", el joc de la contaminació, creat per Jordi Julià, Nereida Mestre i Martí Roma

---
+++ miniPlayerData.js
@@ -0,0 +1,15 @@
+klynt.miniPlayerData = {
+    "fullscreenInfoWording": "This program will launch in fullscreen",
+    "thumbnail": "Medias/Photos/benvinguts-verdalia.jpg",
+    "title": "Verdàlia",
+    "resumePlaybackWording": "Resume playback?",
+    "yesWording": "Yes",
+    "thanksForWatchingWording": "Thanks for watching!",
+    "noWording": "No",
+    "launchAppWording": "Then Launch Project",
+    "analyticsKey": "",
+    "downloadAppWording": "Download App",
+    "redirectToMobileApp": "never",
+    "url": "http://www.verdalia.cat",
+    "description": "El joc de la contaminació"
+}
